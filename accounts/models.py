78045ac8cfdc1f9f910136c79af545c3657c38ec
therealuhlarzoltan <therealuhlarzoltan@gmail.com>
2023-01-22 02:16:02 +0100

    custom user model

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from django.conf import settings
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    birth_date = models.DateField(),
+    country = models.CharField(max_length=2, choices=settings.COUNTRY_CHOICES, default="US"),
+    gender = models.CharField(max_length=6, choices=settings.GENDER_CHOICES, default="M")
+
+    REQUIRED_FIELDS = ['birth_date', 'country', 'gender']
