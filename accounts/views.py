bfc1f2a0fc65f7cd6647d17f61a080650760e81c
therealuhlarzoltan <therealuhlarzoltan@gmail.com>
2023-01-28 16:59:56 +0100

    working user creation frontend/backend views

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
 
+
 from .serializers import UserCreateSerializer
+
 
 # Create your views here.
 class LogOutView(APIView):
@@ -26,10 +28,11 @@ class LogOutView(APIView):
 class RegisterView(APIView):
     authentication_classes = []
     permission_classes = []
-
+    
     def post(self, request):
+        print("Incoming request: ", request.data)
         serializer = UserCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"message":"User created"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
